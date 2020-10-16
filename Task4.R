2f53424068fc6e61875f9842a89c4930598a772b
students <student39@teaching.kof.ethz.ch>
2020-10-16 15:47:00 +0200

    Created code for both sub-tasks

--- Task4.R
+++ Task4.R
@@ -1,16 +1,36 @@
-# Queens: Task 
+# Queens: Task4 
 
 # Load data (separated by semicolon)
-data <- read.csv("intro-survey.csv", sep=";")
-View(data)
+data_survey <- read.csv("intro-survey.csv", sep=";")
+View(data_survey)
 
 # --------------------------------------------
 # Part1: Descriptive statistics
 # --------------------------------------------
 
 # How many students participated in survey
-number_participants <- length(unique(data$id))
+number_participants <- length(unique(data_survey$id))
+number_groups <- length(unique(data_survey$cgroup))
 
 # Summary statistics for each answer
-numerical_variables <- colnames(data)[-c(1,2,21,22)]
-summary(data[,numerical_variables])
+numerical_variables <- colnames(data_survey)[-c(1,2,21,22)]
+descriptive_stats <- summary(data_survey[,numerical_variables])
+
+# --------------------------------------------
+# Part2: Figures
+# --------------------------------------------
+library(ggplot2)
+library(reshape2)
+
+# Long form
+data_survey_melted <- melt(data_survey[,-c(2,21)], id.vars=c("id", "cgroup"))
+
+# Barplot
+plot_overview <- ggplot(data=data_survey_melted, aes(x=factor(value), fill=cgroup))+
+  labs(x="answer")+
+  geom_bar()+
+  facet_grid(. ~ variable)
+
+# --------------------------------------------
+# End
+# --------------------------------------------
