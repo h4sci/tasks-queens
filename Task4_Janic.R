3bd94a7ce45117098c3f05ef3855f4a105b40230
Daniel Kopp <kopp@kof.ethz.ch>
2020-10-21 20:24:15 +0200

    Adding two new graphs to Janics code

--- Task4_Janic.R
+++ Task4_Janic.R
@@ -1,8 +1,12 @@
 # Queens: Task4 
 
+library("dplyr")
+library("tidyverse")
+
 # Load data (separated by semicolon)
 data_survey <- read.csv("intro-survey.csv", sep=";")
 View(data_survey)
+
 
 # --------------------------------------------
 # Part1: Descriptive statistics
@@ -16,11 +20,13 @@ number_groups <- length(unique(data_survey$cgroup))
 numerical_variables <- colnames(data_survey)[-c(1,2,21,22)]
 descriptive_stats <- summary(data_survey[,numerical_variables])
 
+
 # --------------------------------------------
 # Part2: Figures
 # --------------------------------------------
 library(ggplot2)
 library(reshape2)
+
 
 # Long form
 data_survey_melted <- melt(data_survey[,-c(2,21)], id.vars=c("id", "cgroup"))
@@ -30,6 +36,17 @@ plot_overview <- ggplot(data=data_survey_melted, aes(x=factor(value), fill=cgrou
   labs(x="answer")+
   geom_bar()+
   facet_grid(. ~ variable)
+
+
+# Barcharts on some programming languages:
+
+ggplot(data = data_survey) + 
+  geom_bar(mapping = aes(x = l_r)) +
+  labs(title="R", x="level") 
+
+ggplot(data = data_survey) + 
+  geom_bar(mapping = aes(x = l_python)) +
+  labs(title="Python", x="level") 
 
 # --------------------------------------------
 # End
